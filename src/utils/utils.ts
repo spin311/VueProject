c1219da312d5021a77944a999461341d6b2b5787
spin311 <79504899+spin311@users.noreply.github.com>
2024-06-24 09:31:41 +0200

    removed uneccesary files for proxy server

--- src/utils/utils.ts
+++
@@ -1,6 +0,0 @@
-// generate a random globally unique identifier (GUID)
-export function getGuid(): string {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
-  );
-}
